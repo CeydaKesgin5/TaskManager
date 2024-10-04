b0793cf901c04d7d43f5cb1fc7789a6daa09cd9c
Ceydakesgin5 <ceydakesgin778@gmail.com>
2024-10-04 12:22:04 +0300

    Removed DataAccess_ directory.

--- TaskManager/DataAccess_/Abstract/ITaskDal.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaskManagerClass.Core.DataAccess;
-
-namespace TaskManagerClass.DataAccess.Abstract
-{
-    public interface ITaskDal : IEntityRepository<Task>
-    {
-        List<Task> GetByCategoryId(int categoryId);
-    }
-}
